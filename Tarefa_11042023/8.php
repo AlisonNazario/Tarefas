4cf6d33573a2b66bd1f4abbba08e45a745511f4a
Alison Nazario <nazarioalison137@gmail.com>
2023-04-23 11:20:52 -0300

    Exercicio 8 completo

---
+++ Tarefa_11042023/8.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="post">
+        <input type="text" name="palavra" id="palavra">
+        <input type="submit" value="Enviar">
+    </form>
+    <?php
+
+     $palavra = isset($_POST['palavra']) ? $_POST['palavra'] : "";
+
+     function converteMinusculo($palavra) {
+        $palavraMinusculo = strtolower($palavra);
+        return $palavraMinusculo;
+      }
+      
+      echo "A palavra $palavra em minúsculo é " . converteMinusculo($palavra) . "<br>";
+      
+      
+    ?>
+</body>
+</html>
